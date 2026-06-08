13a7485aa4628a360c4fefcb0d9c52bc78bac45c
TylerPane <tyler.paneitz@veteransunited.com>
2026-06-08 09:16:30 -0500

    feat: add Rush, Dodge, and Block skills to Mercenary

--- Scripts/Creatures/Humanoids/Mercenaries/Mercenary.cs
+++ Scripts/Creatures/Humanoids/Mercenaries/Mercenary.cs
@@ -1,3 +1,4 @@
+using RuinGamePDT.Combat;
 using RuinGamePDT.Weapons;
 
 namespace RuinGamePDT.Creatures;
@@ -11,8 +12,51 @@ public class Mercenary : Creature
     {
         EquippedWeapon = new Unarmed();
         Attacks.AddRange(EquippedWeapon.Attacks);
+
+        int mpHalf = CombatStats.MovementPoints / 2;
+
+        Skills.Add(new Skill(
+            name: "Rush",
+            minDamage: 0,
+            maxDamage: 0,
+            actionPointCost: 1,
+            accuracy: 100,
+            attackShape: new AttackShape(new[] { (0, 0) }),
+            range: 0,
+            onHit: new AttackEffect(AttackEffectType.StatIncrease, CombatStat.MovementPoints, mpHalf, mpHalf, 1, 1)
+        ));
+
+        Skills.Add(new Skill(
+            name: "Dodge",
+            minDamage: 0,
+            maxDamage: 0,
+            actionPointCost: 1,
+            accuracy: 100,
+            attackShape: new AttackShape(new[] { (0, 0) }),
+            range: 0,
+            onHit: new AttackEffect(AttackEffectType.StatIncrease, CombatStat.Evasion, 5, 15, 1, 1)
+        ));
+
+        Skills.Add(new Skill(
+            name: "Block",
+            minDamage: 0,
+            maxDamage: 0,
+            actionPointCost: 1,
+            accuracy: 100,
+            attackShape: new AttackShape(new[] { (0, 0) }),
+            range: 0,
+            onHit: new AttackEffect(AttackEffectType.StatIncrease, CombatStat.PhysicalDefense, 5, 15, 1, 1)
+        ));
     }
 
     protected Mercenary(string name, int agility, int focus, int mind, int strength, int stamina)
         : base(name, agility, focus, mind, strength, stamina) { }
+
+    public static Mercenary CreateRandom() => new(
+        agility:  Random.Shared.Next(1, 6),
+        focus:    Random.Shared.Next(1, 6),
+        mind:     Random.Shared.Next(1, 6),
+        strength: Random.Shared.Next(1, 6),
+        stamina:  Random.Shared.Next(1, 6)
+    );
 }
--- Tests/MercenaryTests.cs
+++ Tests/MercenaryTests.cs
@@ -1,3 +1,4 @@
+using RuinGamePDT.Combat;
 using RuinGamePDT.Creatures;
 using RuinGamePDT.Weapons;
 using static RuinGamePDT.Creatures.BaseStat;
@@ -74,5 +75,94 @@ public class MercenaryTests
 
         Assert.Same(weapon, m.EquippedWeapon);
         Assert.Equal(attackCount, m.Attacks.Count);
+    }
+
+    [Fact]
+    public void CreateRandom_AllStatsAreWithinOneToFive()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            var m = Mercenary.CreateRandom();
+            Assert.InRange(m.BaseStats.Agility,  1, 5);
+            Assert.InRange(m.BaseStats.Focus,    1, 5);
+            Assert.InRange(m.BaseStats.Mind,     1, 5);
+            Assert.InRange(m.BaseStats.Strength, 1, 5);
+            Assert.InRange(m.BaseStats.Stamina,  1, 5);
+        }
+    }
+
+    [Fact]
+    public void CreateRandom_ProducesVariedStats()
+    {
+        // With 50 mercs, the chance all five stats on every merc equal 1 is astronomically small.
+        var mercs = Enumerable.Range(0, 50).Select(_ => Mercenary.CreateRandom()).ToList();
+        Assert.Contains(mercs, m => m.BaseStats.Agility  > 1);
+        Assert.Contains(mercs, m => m.BaseStats.Focus    > 1);
+        Assert.Contains(mercs, m => m.BaseStats.Mind     > 1);
+        Assert.Contains(mercs, m => m.BaseStats.Strength > 1);
+        Assert.Contains(mercs, m => m.BaseStats.Stamina  > 1);
+    }
+
+    [Fact]
+    public void Mercenary_HasRushDodgeAndBlock_InSkills()
+    {
+        var m = new Mercenary();
+        Assert.Contains(m.Skills, s => s.Name == "Rush");
+        Assert.Contains(m.Skills, s => s.Name == "Dodge");
+        Assert.Contains(m.Skills, s => s.Name == "Block");
+    }
+
+    [Fact]
+    public void Rush_HasCorrectProperties()
+    {
+        var m = new Mercenary(stamina: 4); // MovementPoints = 4+3 = 7, half = 3
+        var rush = m.Skills.First(s => s.Name == "Rush");
+        Assert.Equal(0, rush.MinDamage);
+        Assert.Equal(0, rush.MaxDamage);
+        Assert.Equal(1, rush.ActionPointCost);
+        Assert.Equal(0, rush.Range);
+        Assert.NotNull(rush.OnHit);
+        Assert.Equal(AttackEffectType.StatIncrease, rush.OnHit!.Type);
+        Assert.Equal(CombatStat.MovementPoints, rush.OnHit.TargetStat);
+        Assert.Equal(3, rush.OnHit.MinAmount);
+        Assert.Equal(3, rush.OnHit.MaxAmount);
+        Assert.Equal(1, rush.OnHit.MinDuration);
+        Assert.Equal(1, rush.OnHit.MaxDuration);
+    }
+
+    [Fact]
+    public void Dodge_HasCorrectProperties()
+    {
+        var m = new Mercenary();
+        var dodge = m.Skills.First(s => s.Name == "Dodge");
+        Assert.Equal(0, dodge.MinDamage);
+        Assert.Equal(0, dodge.MaxDamage);
+        Assert.Equal(1, dodge.ActionPointCost);
+        Assert.Equal(0, dodge.Range);
+        Assert.NotNull(dodge.OnHit);
+        Assert.Equal(AttackEffectType.StatIncrease, dodge.OnHit!.Type);
+        Assert.Equal(CombatStat.Evasion, dodge.OnHit.TargetStat);
+        Assert.Equal(5, dodge.OnHit.MinAmount);
+        Assert.Equal(15, dodge.OnHit.MaxAmount);
+        Assert.Equal(1, dodge.OnHit.MinDuration);
+        Assert.Equal(1, dodge.OnHit.MaxDuration);
+    }
+
+    [Fact]
+    public void Block_HasCorrectProperties()
+    {
+        var m = new Mercenary();
+        var block = m.Skills.First(s => s.Name == "Block");
+        Assert.Equal(0, block.MinDamage);
+        Assert.Equal(0, block.MaxDamage);
+        Assert.Equal(1, block.ActionPointCost);
+        Assert.Equal(0, block.Range);
+        Assert.NotNull(block.OnHit);
+        Assert.Equal(AttackEffectType.StatIncrease, block.OnHit!.Type);
+        Assert.Equal(CombatStat.PhysicalDefense, block.OnHit.TargetStat);
+        Assert.Equal(5, block.OnHit.MinAmount);
+        Assert.Equal(15, block.OnHit.MaxAmount);
+        Assert.Equal(1, block.OnHit.MinDuration);
+        Assert.Equal(1, block.OnHit.MaxDuration);
     }
 }
